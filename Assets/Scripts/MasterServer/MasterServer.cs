61d588296124ca1364c498c71fb024c362457557
Moandor <moandor.y@gmail.com>
2017-11-06 23:10:04 +0800

    Add master server scene

---
+++ Assets/Scripts/MasterServer/MasterServer.cs
@@ -0,0 +1,12 @@
+﻿using Barebones.MasterServer;
+
+namespace Assets.Scripts.MasterServer
+{
+    public class MasterServer : MasterServerBehaviour
+    {
+        protected override void Start()
+        {
+            StartCoroutine(StartOnNextFrame());
+        }
+    }
+}
