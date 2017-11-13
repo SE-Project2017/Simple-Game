f67e1d1ba2f665295b5b2b5067fa682b7af1b49b
Hobbitzx <hgdzx2015@163.com>
2017-11-13 15:07:52 +0800

    MainCamera added

--- Assets/Scripts/App/MainCamera.cs
+++ Assets/Scripts/App/MainCamera.cs
@@ -5,8 +5,26 @@ namespace Assets.Scripts.App
     [ExecuteInEditMode, RequireComponent(typeof(Camera))]
     public class MainCamera : MonoBehaviour
     {
-        public void Awake() { }
+        private Camera mCamera;
 
-        public void Update() { }
+        public void Awake()
+        {
+            mCamera = GetComponent<Camera>();
+        }
+
+        public void Update()
+        {
+            const float aspectRatio = 9.0f / 16.0f;
+            float width = Screen.width;
+            float height = Screen.height;
+            if (width / height >= aspectRatio)
+            {
+                mCamera.orthographicSize = 5.0f;
+            }
+            else
+            {
+                mCamera.orthographicSize = 5.0f / (width / height / aspectRatio);
+            }
+        }
     }
 }
